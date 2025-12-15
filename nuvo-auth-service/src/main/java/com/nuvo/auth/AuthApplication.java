1e4bc87584e2465dda53267cb6aebf3e9abc936d
Sebastian Arnache Cantillo <sarnachecantillo@gmail.com>
2025-12-15 15:20:57 -0500

    Fix Auth Service startup: add explicit entity and repository scanning

--- nuvo-auth-service/src/main/java/com/nuvo/auth/AuthApplication.java
+++ nuvo-auth-service/src/main/java/com/nuvo/auth/AuthApplication.java
@@ -1,8 +1,11 @@
 package com.nuvo.auth;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@org.springframework.boot.autoconfigure.domain.EntityScan(basePackages = "com.nuvo.auth.infrastructure.entities")
+@org.springframework.data.jpa.repository.config.EnableJpaRepositories(basePackages = "com.nuvo.auth.infrastructure.repositories")
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
